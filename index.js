15db00ed1e3e8afee22f7b0383a9748d4f46f27e
Tatiana Mathia <mathiatatiana@gmail.com>
2024-03-28 09:35:19 +0300

    arrays

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(){
+    cats.push("Ralph")
+}
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(){
+    cats.pop ("Ralph")
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift("Bob")
+}
+function appendCat(Broom){
+    return[...cats,Broom]
+}
+function prependCat(Arnold){
+    return[Arnold ,...cats]
+}
+function removeFirstCat(){
+    return cats.slice(1)
+}
+function removeLastCat(){
+    return cats.slice(0,cats.length-1)
+}
